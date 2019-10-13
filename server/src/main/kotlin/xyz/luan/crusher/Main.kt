e62dc7dec37d04da8563e1deb2c6899cd440e783
Luan Nico <luanpotter27@gmail.com>
2019-10-13 17:51:54 -0400

    .

--- server/src/main/kotlin/xyz/luan/crusher/Main.kt
+++ server/src/main/kotlin/xyz/luan/crusher/Main.kt
@@ -2,17 +2,14 @@ package xyz.luan.crusher
 
 import spark.kotlin.*
 
-object Main {
-    private fun getHerokuAssignedPort(): Int {
-        val env = ProcessBuilder().environment()
-        return env["PORT"]?.let { Integer.parseInt(it) } ?: 4567
-    }
+private fun getHerokuAssignedPort(): Int {
+    val env = ProcessBuilder().environment()
+    return env["PORT"]?.let { Integer.parseInt(it) } ?: 4567
+}
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val http: Http = ignite()
-        port(getHerokuAssignedPort())
+fun main() {
+    val http: Http = ignite()
+    port(getHerokuAssignedPort())
 
-        http.get("/hello") { "Hello Spark Kotlin/Heroku!" }
-    }
+    http.get("/hello") { "Hello Spark Kotlin/Heroku!" }
 }
