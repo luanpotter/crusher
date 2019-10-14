c329219c3d9a08a8b37d6ba3a408f85007518c71
Luan Nico <luanpotter27@gmail.com>
2019-10-14 13:28:53 -0400

    .

--- server/cmds/heroku-unpack.sh
+++ server/cmds/heroku-unpack.sh
@@ -2,6 +2,5 @@
 
 IN=src/main/resources/crusher-app-firebase-service-account.json.enc
 OUT=src/main/resources/crusher-app-firebase-service-account.json
-PASSWORD=`heroku config:get SECRET_KEY`
 
-echo $PASSWORD | gpg --batch --yes --passphrase-fd 0 --output $OUT --decrypt $IN
+echo $SECRET_KEY | gpg --batch --yes --passphrase-fd 0 --output $OUT --decrypt $IN
